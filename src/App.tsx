fea7d95d43595c4232873e9871e9a063a982f8cf
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-07-08 18:41:41 -0500

    Se crean el Prop para MenuItem y se itera en el main

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { menuItems } from "./types"
+import MenuItem from "./components/MenuItem"
+import { menuItems } from "./data/db"
 
 function App() {
   
@@ -8,6 +9,24 @@ function App() {
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto mt-20 py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+
+          {menuItems.map(item => (
+            <MenuItem
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </div>
+        
+        <div>
+          <h2>Consumo</h2>
+        </div>
+        
+      </main>
     </>
   )
 }
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,14 @@
+import type { MenuItem } from "../types"
 
-export default function MenuItem() {
+type MenuItemProps = { // Se crea el prop de menu item
+  item: MenuItem
+}
+
+export default function MenuItem({item} : MenuItemProps) { // Se aplica destructuring a la funcion de MenuItem, haciendo que deje de ser de tipo "any"
   return (
-    <div>MenuItem</div>
+    <>
+      <p>{item.name}</p>
+      <p className="font-black">{item.price}</p>
+    </>
   )
 }
