c6c0dabeb02fecd401d0f4538f61101e72cb0324
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-07-10 17:13:20 -0500

    Agregando el signature a la funcion de agregar articulo

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,14 +2,14 @@ import type { MenuItem } from "../types"
 
 type MenuItemProps = { // Se crea el prop de menu item
   item: MenuItem
-  addItem: () => void
+  addItem: (item: MenuItem) => void
 }
 
 export default function MenuItem({item, addItem} : MenuItemProps) { // Se aplica destructuring a la funcion de MenuItem, haciendo que deje de ser de tipo "any"
   return (
     <button
       className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
-      onClick={() => addItem()} // Se agrega el item despues de dar click
+      onClick={() => addItem(item)} // Se agrega el item despues de dar click
     >
     
       <p>{item.name}</p>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,10 +1,11 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([]) // Utilizando el Generic se especifica el tipo de dato que va a utilizar "order" en este caso el type de OrderItem[]
-    const addItem = () => {
-        console.log("Agregando")
+
+    const addItem = (item: MenuItem) => {
+        console.log(item)
     }
 
     return {
