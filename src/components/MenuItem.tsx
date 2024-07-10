8c70513e65d280a1b182d9a1ae670da45eb5df55
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-07-10 16:36:15 -0500

    Listando los productos del menu

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,9 +6,12 @@ type MenuItemProps = { // Se crea el prop de menu item
 
 export default function MenuItem({item} : MenuItemProps) { // Se aplica destructuring a la funcion de MenuItem, haciendo que deje de ser de tipo "any"
   return (
-    <>
+    <button
+      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+    >
+    
       <p>{item.name}</p>
-      <p className="font-black">{item.price}</p>
-    </>
+      <p className="font-black">${item.price}</p>
+    </button>
   )
 }
