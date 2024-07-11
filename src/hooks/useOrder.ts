611fa6dcf9fa18621bdaf33397a31710e0562ec2
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-07-11 15:23:40 -0500

    Eliminando articulos de la orden

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -20,8 +20,13 @@ export default function useOrder() {
         
     }
 
+    const removeItem = (id: MenuItem['id']) => { // Sintaxis de lookUp
+        setOrder(order.filter(item => item.id !== id))
+    }
+
     return {
         order,
-        addItem
+        addItem,
+        removeItem
     }
 }
