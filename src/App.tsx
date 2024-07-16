bdd753e29e500cb2efaf3d6bebe8a040a0951209
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-07-15 21:53:28 -0500

    Añadiendo la logica y el boton para reiniciar la orden

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const { order, addItem, removeItem, tip, setTip } = useOrder()
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
   
 
   return (
@@ -33,20 +33,28 @@ function App() {
         </div>
         
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents
-            order={order}
-            removeItem={removeItem}
-          />
+          {order.length ? ( // Evaluar si la orden tiene algo
+            <>
+              <OrderContents
+                order={order}
+                removeItem={removeItem}
+              />
 
-          <TipPercentageForm
-            setTip={setTip}
-          />
+              <TipPercentageForm
+                setTip={setTip}
+                tip={tip}
+              />
 
-          <OrderTotals
-            order={order}
-            tip={tip}
-          />
+              <OrderTotals
+                order={order}
+                tip={tip}
+                placeOrder={placeOrder}
+              />
+            </>
 
+          ) : ( // Si la orden no tiene nada
+            <p className="text-center">La orden está vacia</p>
+          )}
         </div>
       </main>
     </>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,34 +1,39 @@
-import { useMemo } from "react"
+import { useCallback } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
 type OrderTotalProps = {
     order: OrderItem[],
-    tip: number
+    tip: number,
+    placeOrder: () => void
 }
 
 
-export default function OrderTotals({order, tip}: OrderTotalProps) {
-    const subtotalAmount = useMemo(() => order.reduce((total,item) => total + (item.quantity * item.price), 0), [order])
-    const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
-    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
+export default function OrderTotals({order, tip, placeOrder}: OrderTotalProps) {
+    const subtotalAmount = useCallback(() => order.reduce((total,item) => total + (item.quantity * item.price), 0), [order])
+    const tipAmount = useCallback(() => subtotalAmount() * tip, [tip, order])
+    const totalAmount = useCallback(() => subtotalAmount() + tipAmount(), [tip, order])
 
     return (
         <>
             <div className="space-y-3">
                 <h2 className="font-black text-2xl">Totales y Propina:</h2>
                 <p>Subtotal a pagar: {''}
-                    <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+                    <span className="font-bold">{formatCurrency(subtotalAmount())}</span>
                 </p>
                 <p>Propina: {''}
-                    <span className="font-bold">{formatCurrency(tipAmount)}</span>
+                    <span className="font-bold">{formatCurrency(tipAmount())}</span>
                 </p>
                 <p>Total a pagar: {''}
-                    <span className="font-bold">{formatCurrency(totalAmount)}</span>
+                    <span className="font-bold">{formatCurrency(totalAmount())}</span>
                 </p>
             </div>
-            <button>
-
+            <button
+                className="w-full bg-black p-2 uppercase text-white font-bold mt-10 disabled:opacity-15"
+                disabled={totalAmount() === 0}
+                onClick={placeOrder}
+            >
+                Guardar orden
             </button>
         </> 
     )
